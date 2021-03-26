d8b8587b61304764dea9e217b837e9e2be5a57c6
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 08:22:27 +0530

    added properties of address book

--- Address_book.java
+++ Address_book.java
@@ -1,2 +1,51 @@
-//welcome
+import java.util.Scanner;
 
+public class Address_book {
+    Scanner fetch = new Scanner(System.in);
+
+
+
+
+
+    String first_name;
+    String last_name;
+    long phone_no;
+    String email;
+    int zip_code;
+    String city;
+
+
+    Address_book_1(String f_name, String l_name, long phone, String em, int zip, String city) {
+
+        this.first_name = f_name;
+        this.last_name = l_name;
+        this.phone_no = phone;
+        this.email = em;
+        this.zip_code = zip;
+        this.city = city;
+
+    }
+
+
+
+    public String toString(){
+
+        return ("FIRST NAME : "+first_name+"\nLAST NAME : "+last_name+"\nPHONE : "+phone_no+"\nEMAIL : "+email+"\nZIP CODE : "+zip_code+"\nCITY : "+city);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Address_book_1 salman = new Address_book_1("Salman", "Khan", 1234567890, "salman@khan.com", 456002, "Mumbai");
+        Address_book_1 tom = new Address_book_1("Tom", "Cruise", 789456230, "cruise@usa.com", 123456, "Los Angeles");
+
+
+        System.out.println(salman);
+        System.out.println(tom);
+
+    }
+
+
+
+    }
