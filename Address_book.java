851fea292385e9e7db1a8592d99f20d941f6d044
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 20:01:03 +0530

    takes user input to add entries

--- Address_book.java
+++ Address_book.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Address_book {
@@ -12,20 +14,24 @@ public class Address_book {
     String city;
 
 
-    //Using constructor to pass arguments
-    Address_book(String f_name, String l_name, long phone, String em, int zip, String city) {
-
-        this.first_name = f_name;
-        this.last_name = l_name;
-        this.phone_no = phone;
-        this.email = em;
-        this.zip_code = zip;
-        this.city = city;
+    //Using set Details method to take User input
+    public void set_details() {
+        System.out.println("GIVE ME THE FIRST NAME :");
+        this.first_name = fetch.nextLine();
+        System.out.println("GIVE ME THE LAST NAME :");
+        this.last_name = fetch.nextLine();
+        System.out.println("GIVE ME THE PHONE NO. :");
+        this.phone_no = fetch.nextInt();
+        System.out.println("GIVE ME THE EMAIL: ");
+        this.email = fetch.next();
+        System.out.println("GIVE ME THE ZIP CODE");
+        this.zip_code = fetch.nextInt();
+        System.out.println("GIVE ME THE CITY :");
+        this.city = fetch.next();
 
     }
 
-
-    // Overriding toString Method to display output
+    // To display output
     public String toString() {
 
         return ("FIRST NAME : " + first_name + "\nLAST NAME : " + last_name + "\nPHONE : " + phone_no + "\nEMAIL : " + email + "\nZIP CODE : " + zip_code + "\nCITY : " + city);
@@ -34,18 +40,40 @@ public class Address_book {
 
     public static void main(String[] args) {
 
+        //Creating a list of class Address_book
+        List<Address_book> users = new ArrayList<>();
+        Address_book salman = new Address_book();
+        Address_book tom = new Address_book();
+        System.out.println("GIVE ME SALMAN's DETAILS");
+        salman.set_details();
+        salman.set_details();
+        System.out.println("GIVE ME TOM's DETAILS");
+        tom.set_details();
 
-        
-        // Creating salman and tom instances from address_book class
-        Address_book salman = new Address_book("Salman", "Khan", 1234567890, "salman@khan.com", 456002, "Mumbai");
-        Address_book tom = new Address_book("Tom", "Cruise", 789456230, "cruise@usa.com", 123456, "Los Angeles");
+        users.add(salman);
+        users.add(tom);
+        Scanner input = new Scanner(System.in);
+        System.out.println("WHAT DO YOU WANT TO DO \n" + "1.ADD DETAILS\n 2.EXIT");
+        int choice = input.nextInt();
+        //using switch make appropraite choices
+        switch (choice) {
+            //Adding the user case
+            case 1:
 
-        // printing salman and tom objects
-        System.out.println(salman);
-        System.out.println(tom);
+                Address_book steve = new Address_book();
+                steve.set_details();
+                users.add(steve);
+                System.out.println("Added THE USER :");
+                System.out.println(users);
+
+
+            default:
+                break;
+
+        }
+
 
     }
-
 
 
 }
